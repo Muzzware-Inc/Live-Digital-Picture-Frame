072d56d43059897b822efc356ed1b4c354edf414
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-19 13:09:39 +0100

    added states for switch

--- src/switch.py
+++ src/switch.py
@@ -13,3 +13,22 @@ device = uinput.Device([
         uinput.KEY_N,
         uinput.KEY_F5,
         ])
+
+while True:
+    input_state_back = GPIO.input(19)
+    input_state_forward = GPIO.input(20) 
+    input_state_refresh = GPIO.input(21)
+
+    if input_state_back == False:
+        print('Button P Pressed')
+        device.emit_click(uinput.KEY_P)
+        time.sleep(0.5)
+    if input_state_forward == False:
+        print('Button N Pressed')
+        device.emit_click(uinput.KEY_N)
+        time.sleep(0.5)
+    if input_state_refresh == False:
+        print('Button F5 Pressed')
+        device.emit_click(uinput.KEY_F5)
+        time.sleep(0.5)
+

32a1c5b93a3029bf91392cec50b5728be3f6f96c
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-19 13:06:47 +0100

    modified switch.py and extended

--- src/switch-extended.py
+++ src/switch-extended.py
@@ -1,3 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 import uinput
+import os
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+device = uinput.Device([
+        uinput.KEY_P,
+        uinput.KEY_N,
+	uinput.KEY_F5,
+	uinput.KEY_M,
+	uinput.KEY_A
+        ])
+
+view = 'm'
